ad229f57e5be712dc1565e30edfd5c01c4592496
Bobby Odian <odianrobert@yahoo.com>
2020-02-02 14:01:25 -0500

    added routes, front end files, server and html pages working

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+
+//Routes
+
+app.get("/notes", function(req, res) {
+    // var data = JSON.parse(fs.readFileSync('db.json', 'utf8'));
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+  });
+// put /* at bottom
+  
+app.get("/api/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "./public/Develop/journal.json"));
+  });
+
+  app.get("/*", function(req, res) {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+  });
+
+  //Post
+
+  app.post("/api/notes", function(req, res) {
+
+  }
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+
+  
